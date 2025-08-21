d103d602da968e55ff09f14ea9a5fd2ca1cd5eb3
mossaberrahal02 <mossaberrahal@gmail.com>
2025-08-21 10:01:18 +0100

    core server "multiplexing"

--- server.cpp
+++ server.cpp
@@ -1,20 +1,53 @@
 #include "server.hpp"
+#include <iostream>
+#include <sys/socket.h>
 
 Server::Server() {
     std::cout << "Server initialized." << std::endl;
-}Server::Server(int p) : port(p) {
+}
+Server::Server(int port) : port(port) {
     std::cout << "Server initialized on port " << port << "." << std::endl;
 }
-Server::Server(const Server& other) : port(other.port) {
+Server::Server(int port, const std::string& password) : port(port), password(password) 
+{
+}
+Server::Server(const Server& other) : port(other.port), password(other.password) {
 }
 Server& Server::operator=(const Server& other) {
     if (this != &other) {
         port = other.port;
+        password = other.password;
     }
     return *this;
 }
 
 void Server::start() {
+    std::cout << "Password: " << password << std::endl;
+    std::cout << "port: " << port << std::endl;
+
+    serverSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (serverSocket == 0) {
+        std::cerr << "Socket creation failed." << std::endl;
+        return;
+    }
+    int opt = 1;
+    if (setsockopt(serverSocket, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt)) < 0) {
+        std::cerr << "Setsockopt failed." << std::endl;
+        return;
+    }
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(port);
+
+    if (bind(serverSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) < 0) {
+        std::cerr << "Bind failed." << std::endl;
+        return;
+    }
+
+    if (listen(serverSocket, 3) < 0) {
+        std::cerr << "Listen failed." << std::endl;
+        return;
+    }
     std::cout << "Server started. Listening on port " << port << "..." << std::endl;
 }
 
--- server.hpp
+++ server.hpp
@@ -1,22 +1,26 @@
 #pragma once
-#include <iostream>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <string>
+#include <cstdlib>
 
 class Server {
-    int port;
-    int serverSocket, newSocket;
-    struct sockaddr_in address;
-public:
-    Server();
-    ~Server();
-    Server(int p);
-    Server(const Server& other);
-    Server& operator=(const Server& other);
-    
-    void setPort(int p);
-    int getPort() const;
+    private:
+        int port;
+        int serverSocket, newSocket;
+        struct sockaddr_in serverAddress;
+        std::string password;
+    public:
+        Server();
+        ~Server();
+        Server(int p);
+        Server(int port, const std::string& password);
+        Server(const Server& other);
+        Server& operator=(const Server& other);
+        
+        void setPort(int p);
+        int getPort() const;
 
-    void start();
-    void stop();
+        void start();
+        void stop();
 };
