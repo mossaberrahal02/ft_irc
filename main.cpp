7674e6b24e790c49f4cc9aedcb52ed3daa2578fb
mossaberrahal02 <mossaberrahal@gmail.com>
2025-08-21 10:01:43 +0100

    parse port and password

--- main.cpp
+++ main.cpp
@@ -1,7 +1,34 @@
 #include "server.hpp"
+#include <iostream>
 
+int isValidPort(int port, const char* portStr) {
+    if ( port < 1024 || port > 65535 ) 
+        return 0;
+    for(int i = 0; portStr[i] != '\0'; i++) {
+        if (!isdigit(portStr[i])) 
+            return 0;
+    }
+    return 1;
+}
 
-int main()
+int main(int argc, char* argv[])
 {
+    if(argc != 3)
+    {
+        std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
+        return 1;
+    }
+
+    int port = atoi(argv[1]);
+    std::string password = argv[2];
+
+    if(!isValidPort(port, argv[1]))
+    {
+        std::cerr << "Invalid port number: " << argv[1] << std::endl;
+        return 1;
+    }
+
+    Server server(port, password);
+    server.start();
     return 0;
 }
