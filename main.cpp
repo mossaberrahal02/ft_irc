ab20007d1f441985b28d0b3d9db036044c151b04
mossaberrahal02 <mossaberrahal@gmail.com>
2025-08-22 10:14:52 +0100

    main programe

--- main.cpp
+++ main.cpp
@@ -14,21 +14,12 @@ int isValidPort(int port, const char* portStr) {
 int main(int argc, char* argv[])
 {
     if(argc != 3)
-    {
-        std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;
-        return 1;
-    }
-
-    int port = atoi(argv[1]);
-    std::string password = argv[2];
-
-    if(!isValidPort(port, argv[1]))
-    {
-        std::cerr << "Invalid port number: " << argv[1] << std::endl;
-        return 1;
-    }
-
-    Server server(port, password);
-    server.start();
+        {std::cerr << "Usage: " << argv[0] << " <port> <password>" << std::endl;return 1;}
+    if(!isValidPort(atoi(argv[1]), argv[1]))
+        {std::cerr << "Invalid port number: " << argv[1] << std::endl;return 1;}
+    Server server(atoi(argv[1]), argv[2]);
+    server.initServerSocket();
+    std::cout << "server " << server.getServerSocket() << " is running on port " << server.getPort() << " with password " << server.getPassword() << std::endl;
+    server.coreServerLoop();
     return 0;
 }
